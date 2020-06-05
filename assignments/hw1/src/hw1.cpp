cec77b8fe4edbcee5e28f785f877900e6d809fc6
David Alves <david.alves@dfinity.org>
2020-06-04 19:02:47 -0700

    Assignment 1
    This contains started code and example installation instructions
    to get started with HW1.

---
+++ assignments/hw1/src/hw1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//#include <omp.h>
+#include "hw1.h"
+
+double euclidean_distance(std::vector<double> vector) {
+  return 0;
+}
+std::vector<int64_t> discard_duplicates(std::vector<int64_t> sorted_vector) {
+  return std::vector<int64_t>();
+}
