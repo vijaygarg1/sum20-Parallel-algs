16b00c8af20ab5be79e6b513067fc30bcdc4db87
David Alves <david.alves@dfinity.org>
2020-06-23 20:59:46 -0700

    Pull more code from gonum

--- assignments/hw2/go.mod
+++ assignments/hw2/go.mod
@@ -11,7 +11,7 @@ require (
 	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
 	github.com/gonum/lapack v0.0.0-20181123203213-e4cdc5a0bff9 // indirect
 	github.com/gonum/mathext v0.0.0-20181121095525-8a4bf007ea55 // indirect
-	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9 // indirect
+	github.com/gonum/matrix v0.0.0-20181209220409-c518dec07be9
 	github.com/gonum/stat v0.0.0-20181125101827-41a0da705a5b // indirect
 	golang.org/x/tools v0.0.0-20200619210111-0f592d2728bb // indirect
 )
--- assignments/hw2/hw2.go
+++ assignments/hw2/hw2.go
@@ -2,20 +2,31 @@ package hw2
 
 import (
 	"github.com/gonum/graph"
-	"github.com/gonum/graph/path"
 )
 
-func BellmanFord(s graph.Node, g graph.Graph) path.Shortest {
+// Apply the bellman-ford algorihtm to Graph and return
+// a shortest path tree.
+//
+// Note that this uses path.Shortest from the gonum package
+// to make it easier for you, but you can use another struct
+// if that makes more sense for the concurrency model you chose.
+func BellmanFord(s graph.Node, g graph.Graph) Shortest {
 	// Your code goes here.
-	return path.Shortest{}
+	return newShortestFrom(s, g.Nodes())
 }
 
-func DeltaStep(s graph.Node, g graph.Graph) path.Shortest {
+// Apply the delta-stepping algorihtm to Graph and return
+// a shortest path tree.
+//
+// Note that this uses path.Shortest from the gonum package
+// to make it easier for you, but you can use another struct
+// if that makes more sense for the concurrency model you chose.
+func DeltaStep(s graph.Node, g graph.Graph) Shortest {
 	// Your code goes here.
-	return path.Shortest{}
+	return newShortestFrom(s, g.Nodes())
 }
 
 // Runs dijkstra from gonum to make sure that the tests are correct.
-func Dijkstra(s graph.Node, g graph.Graph) path.Shortest {
-	return path.DijkstraFrom(s, g)
+func Dijkstra(s graph.Node, g graph.Graph) Shortest {
+	return DijkstraFrom(s, g)
 }
